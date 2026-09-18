f6fae5765a523766e0b3f10972c14d5be8d1cb58
axellegbls <axelle.goebels@gmail.com>
2026-09-18 18:07:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const form = document.getElementById("eventForm");
 button.addEventListener("click", function() {
     form.style.display = "flex";
 });
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById("eventName").value;
+    const date = document.getElementById("eventDate").value;
+    const place = document.getElementById("eventPlace").value;
+    const city = document.getElementById("eventCity").value;
+
+    alert(
+        "Événement ajouté ! 🎉\n\n" +
+        name + "\n" +
+        date + "\n" +
+        place + " — " + city
+    );
+
+});
